602781e7d1b0afa4ffa8a6ed6cf5779a26978a66
Stijn Strickx <stijn.strickx@gmail.com>
2016-01-17 13:10:22 +0100

    Fixes #28 Zurich time zone

--- src/yahoofinance/exchanges/ExchangeTimeZone.java
+++ src/yahoofinance/exchanges/ExchangeTimeZone.java
@@ -67,6 +67,7 @@ public class ExchangeTimeZone {
         SUFFIX_TIMEZONES.put("MA", TimeZone.getTimeZone("Europe/Madrid"));
         SUFFIX_TIMEZONES.put("ST", TimeZone.getTimeZone("Europe/Stockholm"));
         SUFFIX_TIMEZONES.put("SW", TimeZone.getTimeZone("Europe/Zurich"));
+        SUFFIX_TIMEZONES.put("Z", TimeZone.getTimeZone("Europe/Zurich"));
         SUFFIX_TIMEZONES.put("TWO", TimeZone.getTimeZone("Asia/Taipei"));
         SUFFIX_TIMEZONES.put("TW", TimeZone.getTimeZone("Asia/Taipei"));
         SUFFIX_TIMEZONES.put("L", TimeZone.getTimeZone("Europe/London"));
