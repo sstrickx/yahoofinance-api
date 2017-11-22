e517ab124b391db39621e1d27920b971e5973fb1
Vadim Jouk <Vadim.Jouk@rewe-group.com>
2017-11-22 10:15:16 +0100

    getDividend improvement

--- src/main/java/yahoofinance/quotes/query1v7/StockQuotesQuery1V7Request.java
+++ src/main/java/yahoofinance/quotes/query1v7/StockQuotesQuery1V7Request.java
@@ -1,6 +1,10 @@
 package yahoofinance.quotes.query1v7;
 
+import java.math.BigDecimal;
+import java.util.TimeZone;
+
 import com.fasterxml.jackson.databind.JsonNode;
+
 import yahoofinance.Stock;
 import yahoofinance.Utils;
 import yahoofinance.exchanges.ExchangeTimeZone;
@@ -8,140 +12,136 @@ import yahoofinance.quotes.stock.StockDividend;
 import yahoofinance.quotes.stock.StockQuote;
 import yahoofinance.quotes.stock.StockStats;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.TimeZone;
-
 /**
  *
  * @author Stijn Strickx
  */
 public class StockQuotesQuery1V7Request extends QuotesRequest<Stock> {
 
-    private static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
-    
-    public StockQuotesQuery1V7Request(String symbols) {
-        super(symbols);
-    }
+	private static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
 
-    @Override
-    protected Stock parseJson(JsonNode node) {
-        String symbol = node.get("symbol").asText();
-        Stock stock = new Stock(symbol);
+	public StockQuotesQuery1V7Request(String symbols) {
+		super(symbols);
+	}
 
-        if(node.has("longName")) {
-            stock.setName(node.get("longName").asText());
-        } else {
-            stock.setName(getStringValue(node, "shortName"));
-        }
+	@Override
+	protected Stock parseJson(JsonNode node) {
+		String symbol = node.get("symbol").asText();
+		Stock stock = new Stock(symbol);
 
-        stock.setCurrency(getStringValue(node, "currency"));
-        stock.setStockExchange(getStringValue(node, "fullExchangeName"));
+		if (node.has("longName")) {
+			stock.setName(node.get("longName").asText());
+		} else {
+			stock.setName(this.getStringValue(node, "shortName"));
+		}
 
-        stock.setQuote(this.getQuote(node));
-        stock.setStats(this.getStats(node));
-        stock.setDividend(this.getDividend(node));
+		stock.setCurrency(this.getStringValue(node, "currency"));
+		stock.setStockExchange(this.getStringValue(node, "fullExchangeName"));
 
-        return stock;
-    }
+		stock.setQuote(this.getQuote(node));
+		stock.setStats(this.getStats(node));
+		stock.setDividend(this.getDividend(node));
 
-    private String getStringValue(JsonNode node, String field) {
-        if(node.has(field)) {
-            return node.get(field).asText();
-        }
-        return null;
-    }
+		return stock;
+	}
 
+	private String getStringValue(JsonNode node, String field) {
+		if (node.has(field)) {
+			return node.get(field).asText();
+		}
+		return null;
+	}
 
+	private StockQuote getQuote(JsonNode node) {
+		String symbol = node.get("symbol").asText();
+		StockQuote quote = new StockQuote(symbol);
 
-    private StockQuote getQuote(JsonNode node) {
-        String symbol = node.get("symbol").asText();
-        StockQuote quote = new StockQuote(symbol);
+		quote.setPrice(Utils.getBigDecimal(this.getStringValue(node, "regularMarketPrice")));
+		// quote.setLastTradeSize(null);
+		quote.setAsk(Utils.getBigDecimal(this.getStringValue(node, "ask")));
+		quote.setAskSize(Utils.getLong(this.getStringValue(node, "askSize")));
+		quote.setBid(Utils.getBigDecimal(this.getStringValue(node, "bid")));
+		quote.setBidSize(Utils.getLong(this.getStringValue(node, "bidSize")));
+		quote.setOpen(Utils.getBigDecimal(this.getStringValue(node, "regularMarketOpen")));
+		quote.setPreviousClose(Utils.getBigDecimal(this.getStringValue(node, "regularMarketPreviousClose")));
+		quote.setDayHigh(Utils.getBigDecimal(this.getStringValue(node, "regularMarketDayHigh")));
+		quote.setDayLow(Utils.getBigDecimal(this.getStringValue(node, "regularMarketDayLow")));
 
-        quote.setPrice(Utils.getBigDecimal(getStringValue(node,"regularMarketPrice")));
-        // quote.setLastTradeSize(null);
-        quote.setAsk(Utils.getBigDecimal(getStringValue(node,"ask")));
-        quote.setAskSize(Utils.getLong(getStringValue(node,"askSize")));
-        quote.setBid(Utils.getBigDecimal(getStringValue(node,"bid")));
-        quote.setBidSize(Utils.getLong(getStringValue(node,"bidSize")));
-        quote.setOpen(Utils.getBigDecimal(getStringValue(node,"regularMarketOpen")));
-        quote.setPreviousClose(Utils.getBigDecimal(getStringValue(node,"regularMarketPreviousClose")));
-        quote.setDayHigh(Utils.getBigDecimal(getStringValue(node,"regularMarketDayHigh")));
-        quote.setDayLow(Utils.getBigDecimal(getStringValue(node,"regularMarketDayLow")));
+		if (node.has("exchangeTimezoneName")) {
+			quote.setTimeZone(TimeZone.getTimeZone(node.get("exchangeTimezoneName").asText()));
+		} else {
+			quote.setTimeZone(ExchangeTimeZone.getStockTimeZone(symbol));
+		}
 
-        if(node.has("exchangeTimezoneName")) {
-            quote.setTimeZone(TimeZone.getTimeZone(node.get("exchangeTimezoneName").asText()));
-        } else {
-            quote.setTimeZone(ExchangeTimeZone.getStockTimeZone(symbol));
-        }
+		if (node.has("regularMarketTime")) {
+			quote.setLastTradeTime(Utils.unixToCalendar(node.get("regularMarketTime").asLong()));
+		}
 
-        if(node.has("regularMarketTime")) {
-            quote.setLastTradeTime(Utils.unixToCalendar(node.get("regularMarketTime").asLong()));
-        }
+		quote.setYearHigh(Utils.getBigDecimal(this.getStringValue(node, "fiftyTwoWeekHigh")));
+		quote.setYearLow(Utils.getBigDecimal(this.getStringValue(node, "fiftyTwoWeekLow")));
+		quote.setPriceAvg50(Utils.getBigDecimal(this.getStringValue(node, "fiftyDayAverage")));
+		quote.setPriceAvg200(Utils.getBigDecimal(this.getStringValue(node, "twoHundredDayAverage")));
 
-        quote.setYearHigh(Utils.getBigDecimal(getStringValue(node,"fiftyTwoWeekHigh")));
-        quote.setYearLow(Utils.getBigDecimal(getStringValue(node,"fiftyTwoWeekLow")));
-        quote.setPriceAvg50(Utils.getBigDecimal(getStringValue(node,"fiftyDayAverage")));
-        quote.setPriceAvg200(Utils.getBigDecimal(getStringValue(node,"twoHundredDayAverage")));
+		quote.setVolume(Utils.getLong(this.getStringValue(node, "regularMarketVolume")));
+		quote.setAvgVolume(Utils.getLong(this.getStringValue(node, "averageDailyVolume3Month")));
 
-        quote.setVolume(Utils.getLong(getStringValue(node,"regularMarketVolume")));
-        quote.setAvgVolume(Utils.getLong(getStringValue(node,"averageDailyVolume3Month")));
+		return quote;
+	}
 
-        return quote;
-    }
+	private StockStats getStats(JsonNode node) {
+		String symbol = this.getStringValue(node, "symbol");
+		StockStats stats = new StockStats(symbol);
 
-    private StockStats getStats(JsonNode node) {
-        String symbol = getStringValue(node,"symbol");
-        StockStats stats = new StockStats(symbol);
+		stats.setMarketCap(Utils.getBigDecimal(this.getStringValue(node, "marketCap")));
+		// stats.setSharesFloat(Utils.getLong(getStringValue(node,"sharesOutstanding")));
+		stats.setSharesOutstanding(Utils.getLong(this.getStringValue(node, "sharesOutstanding")));
+		// stats.setSharesOwned(Utils.getLong(getStringValue(node,"symbol")));
 
-        stats.setMarketCap(Utils.getBigDecimal(getStringValue(node,"marketCap")));
-        // stats.setSharesFloat(Utils.getLong(getStringValue(node,"sharesOutstanding")));
-        stats.setSharesOutstanding(Utils.getLong(getStringValue(node,"sharesOutstanding")));
-        // stats.setSharesOwned(Utils.getLong(getStringValue(node,"symbol")));
+		stats.setEps(Utils.getBigDecimal(this.getStringValue(node, "epsTrailingTwelveMonths")));
+		stats.setPe(Utils.getBigDecimal(this.getStringValue(node, "trailingPE")));
+		// stats.setPeg(Utils.getBigDecimal(getStringValue(node,"symbol")));
 
-        stats.setEps(Utils.getBigDecimal(getStringValue(node,"epsTrailingTwelveMonths")));
-        stats.setPe(Utils.getBigDecimal(getStringValue(node,"trailingPE")));
-        // stats.setPeg(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		stats.setEpsEstimateCurrentYear(Utils.getBigDecimal(this.getStringValue(node, "epsForward")));
+		// stats.setEpsEstimateNextQuarter(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		// stats.setEpsEstimateNextYear(Utils.getBigDecimal(getStringValue(node,"symbol")));
 
-        stats.setEpsEstimateCurrentYear(Utils.getBigDecimal(getStringValue(node,"epsForward")));
-        // stats.setEpsEstimateNextQuarter(Utils.getBigDecimal(getStringValue(node,"symbol")));
-        // stats.setEpsEstimateNextYear(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		stats.setPriceBook(Utils.getBigDecimal(this.getStringValue(node, "priceToBook")));
+		// stats.setPriceSales(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		stats.setBookValuePerShare(Utils.getBigDecimal(this.getStringValue(node, "bookValue")));
 
-        stats.setPriceBook(Utils.getBigDecimal(getStringValue(node,"priceToBook")));
-        // stats.setPriceSales(Utils.getBigDecimal(getStringValue(node,"symbol")));
-        stats.setBookValuePerShare(Utils.getBigDecimal(getStringValue(node,"bookValue")));
+		// stats.setOneYearTargetPrice(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		// stats.setEBITDA(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		// stats.setRevenue(Utils.getBigDecimal(getStringValue(node,"symbol")));
 
-        // stats.setOneYearTargetPrice(Utils.getBigDecimal(getStringValue(node,"symbol")));
-        // stats.setEBITDA(Utils.getBigDecimal(getStringValue(node,"symbol")));
-        // stats.setRevenue(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		// stats.setShortRatio(Utils.getBigDecimal(getStringValue(node,"symbol")));
 
-        // stats.setShortRatio(Utils.getBigDecimal(getStringValue(node,"symbol")));
+		if (node.has("earningsTimestamp")) {
+			stats.setEarningsAnnouncement(Utils.unixToCalendar(node.get("earningsTimestamp").asLong()));
+		}
 
-        if(node.has("earningsTimestamp")) {
-            stats.setEarningsAnnouncement(Utils.unixToCalendar(node.get("earningsTimestamp").asLong()));
-        }
+		return stats;
+	}
 
-        return stats;
-    }
+	private StockDividend getDividend(JsonNode node) {
+		String symbol = this.getStringValue(node, "symbol");
+		StockDividend dividend = new StockDividend(symbol);
 
-    private StockDividend getDividend(JsonNode node) {
-        String symbol = getStringValue(node,"symbol");
-        StockDividend dividend = new StockDividend(symbol);
+		if (node.has("dividendDate")) {
+			long dividendTimestamp = node.get("dividendDate").asLong();
+			dividend.setPayDate(Utils.unixToCalendar(dividendTimestamp));
+			// dividend.setExDate(Utils.unixToCalendar(node.get("dividendDate").asLong()));
+		}
+		if (node.has("trailingAnnualDividendRate")) {
+			dividend.setAnnualYield(Utils.getBigDecimal(this.getStringValue(node, "trailingAnnualDividendRate")));
+		}
+		if (node.has("trailingAnnualDividendYield")) {
+			BigDecimal yield = Utils.getBigDecimal(this.getStringValue(node, "trailingAnnualDividendYield"));
+			if (yield != null) {
+				dividend.setAnnualYieldPercent(yield.multiply(ONE_HUNDRED));
+			}
+		}
 
-        if(!node.has("dividendDate")) {
-            return dividend;
-        }
-
-        long dividendTimestamp = node.get("dividendDate").asLong();
-        dividend.setPayDate(Utils.unixToCalendar(dividendTimestamp));
-        // dividend.setExDate(Utils.unixToCalendar(node.get("dividendDate").asLong()));
-        dividend.setAnnualYield(Utils.getBigDecimal(getStringValue(node,"trailingAnnualDividendRate")));
-        BigDecimal yield = Utils.getBigDecimal(getStringValue(node,"trailingAnnualDividendYield"));
-        if(yield != null) {
-            dividend.setAnnualYieldPercent(yield.multiply(ONE_HUNDRED));
-        }
-
-        return dividend;
-    }
+		return dividend;
+	}
 
 }
