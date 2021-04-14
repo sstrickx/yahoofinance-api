5eaa7ee3133a735b37f798c55e55457651f6ab22
thegoat <ruppatel11599@gmail.com>
2021-04-14 13:35:46 -0400

    Connected to yahoo market, had some Logger Error

---
+++ src/main/java/yahoofinance/Main.java
@@ -0,0 +1,30 @@
+package yahoofinance;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        Stock stock = YahooFinance.get("INTC");
+
+        BigDecimal price = stock.getQuote().getPrice();
+        BigDecimal change = stock.getQuote().getChangeInPercent();
+        BigDecimal peg = stock.getStats().getPeg();
+        BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+
+        stock.print();
+
+
+
+        Stock stock2 = YahooFinance.get("AAPL");
+
+        BigDecimal price2 = stock.getQuote().getPrice();
+        BigDecimal change2 = stock.getQuote().getChangeInPercent();
+        BigDecimal peg2 = stock.getStats().getPeg();
+        BigDecimal dividend2 = stock.getDividend().getAnnualYieldPercent();
+
+        stock2.print();
+    }
+}
