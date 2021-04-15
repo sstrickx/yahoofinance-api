228194b213f4c2cac1d7fea9859af992851e484e
PraveshPatel <ppatel@ithaca.edu>
2021-04-15 11:29:47 -0400

    created agent file

---
+++ src/main/Agent Folder /RuleBasedAgent.java
@@ -0,0 +1,19 @@
+public class RuleBasedAgent implements StockAgent{
+
+    //watch list of stocks 
+    //List of stocks it owns
+        //value of the stocks 
+        //amount of stocks per company it owns 
+    
+    //getPortfolio of stocks 
+   
+    //start buying power "balance" (money it starts with) - cannot buy if balance is less than zero 
+    
+    //function that buys stock - cannot buy stock if it can't afford it 
+        //amount of shares bought is a percentage of our buying power "balance" 
+    
+    //function that sells stock
+        //if stock goes up to a certain percentage from bought date 
+        //if stock plumets pass buying price (certain percentage) 
+
+}
