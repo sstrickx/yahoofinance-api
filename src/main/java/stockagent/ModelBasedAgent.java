5adf403ea7aaf4fbe0ad4d860b7ec080347ef5cb
thegoat <ruppatel11599@gmail.com>
2021-04-30 10:49:26 -0400

    trying stuff

--- src/main/java/stockagent/ModelBasedAgent.java
+++ src/main/java/stockagent/ModelBasedAgent.java
@@ -2,11 +2,13 @@ package stockagent;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import yahoofinance.Stock;
 import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
 
 public class ModelBasedAgent implements StockAgent {
 
@@ -15,22 +17,57 @@ public class ModelBasedAgent implements StockAgent {
     public Stock chooseStock( MarketSensor sensor) throws IOException {
         // TODO Auto-generated method stub
 
+
+
+
+
+        Map<Stock, List<HistoricalQuote>> data = new HashMap<Stock, List<HistoricalQuote>>();
+
+
         double value = 0;
         double greatestChange = 0;
         Stock bestStock = new Stock(null);
 
         Map <String, Stock> stocks = sensor.getStocks();
+
+
+
         //System.out.println(stocks);
         
-        for (Map.Entry<String, Stock> entry : stocks.entrySet()){
-            value = entry.getValue().getQuote().getChange().doubleValue();
-            double stockprice = sensor.getStockPrice(entry.getKey()).doubleValue();
-            System.out.println(stockprice);
-            if (value > greatestChange){
-                bestStock = entry.getValue();
-            }
-            //System.out.println(entry);
+        for (Map.Entry<String, Stock> entry : stocks.entrySet()) {
+
+            data.put(entry.getValue(), sensor.getHistory(entry.getKey()));
+
+
         }
+
+
+        for (Map.Entry<Stock, List<HistoricalQuote>> entry : data.entrySet()) {
+            for (int i = 0; i < entry.getValue().size(); i++) {
+                double currValue = entry.getValue().get(i).getClose().doubleValue();
+                //System.out.println(currValue);
+
+                value = entry.getKey().getQuote().getChange().doubleValue();
+
+                System.out.println(value);
+
+
+                if (value > greatestChange){
+                    bestStock = entry.getKey();
+                }
+
+        }
+
+        }
+
+
+
+
+
+            //System.out.println(entry);
+
+
+
         return bestStock;
     }
 
